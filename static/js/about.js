bed182e3994403d48efa68c2a001849f55329b07
Augustin Godinot <agodinot@hotmail.fr>
2015-06-08 18:42:14 +0200

    Travail sur le header du about, surement à adapter à la page d'accueil

--- static/js/about.js
+++ static/js/about.js
@@ -1,15 +1,30 @@
 $(function() {
     // make the logo bigger and the link to the home page smaller
     $('header #logo').removeClass('logo');
-    $('header a').removeClass('full-container-size').addClass('full-container-width');
+    $('header a').removeClass('full-container-size');
     
-    var header = $('#miew')
+    var headerInitialHeight = $('header').height(),
+        logoInitialHeight = $('#logo').height();
     
     $(window).scroll(function() {
-        var windowHeight = document.body.clientHeight,
-            currentScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var windowHeight = $(window).height(),
+            currentScroll = $(window).scrollTop();
         
-        header.addClass('fixed');
+        console.log($('#catchphrase').height());
+        
+        if (headerInitialHeight-currentScroll >= 170){ // Big header
+            $('header').height(headerInitialHeight-currentScroll);
+            $('header a').css('background-color', 'rgba(58, 107, 170, 0.38)');
+            
+            $('#catchphrase').show();
+            $('header img').removeClass('img-fixed');
+        } else if (headerInitialHeight-currentScroll <= 170) { //Small header
+            $('header a').css('background-color', '#3A6BAA');
+            
+            $('#catchphrase').hide();            
+            $('header img').addClass('img-fixed');
+            $('header').height($('header img').height()+10);
+        }        
     });
 });
 
