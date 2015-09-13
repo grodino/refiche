fbf655535f5c883d7f0c89e2be8cab36cc57c8f4
ingegus <agodinot@hotmail.fr>
2015-09-13 17:50:46 +0200

    Inscription avec facebook finie

--- static/js/facebook.js
+++ static/js/facebook.js
@@ -66,7 +66,7 @@ $(function() {
 
     $.getScript('//connect.facebook.net/fr_FR/sdk.js', function(){
         FB.init({
-          appId: '415432081987687',
+          appId: '413837768813785',
           version: 'v2.4'
         });
 
